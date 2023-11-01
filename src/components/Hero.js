85a0d9bc11029d6e1a4ed65d8fe2adf0a1e50229
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-01 20:09:14 +0530

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react"
+import photo from "./images/photo-grid.png"
+
+function Hero (){
+    return (
+        <section className="hero">
+            <img src= {photo} alt="pic-grid" className="hero--image"/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+} 
+
+export default Hero
