0d5fd2b0ab76f874a1c394fedf005a681267259d
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-07 00:33:00 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,10 @@
-import katieImage from './images/katie-zaferes.png';
-import weddingImage from './images/wedding-photography.png';
-import mountainBikeImage from './images/mountain-bike.png';
-
-const data =[
+const data = [
     {
         id: 1,
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: katieImage,
+        coverImg: "katie-zaferes.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -21,7 +17,7 @@ const data =[
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: weddingImage,
+        coverImg: "wedding-photography.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -34,7 +30,7 @@ const data =[
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: mountainBikeImage,
+        coverImg: "mountain-bike.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
