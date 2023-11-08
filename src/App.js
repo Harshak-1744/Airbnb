1d0cd45853a815cb40727e1cbbe871507968447d
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-08 21:47:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero'
+import Hero from './components/Hero';
+import Card from './components/Card';
+import data from './data';
+
+
 
 function App() {
+  const cards = data.map(item => {
+    return (
+        <Card 
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+            openSpots={item.openSpots}
+        />
+    )
+}) 
   return (
     <div>
       <Navbar />
       <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
-  );
+  ); 
 }
 
 export default App;
